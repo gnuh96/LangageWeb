94e5e4bf17e6f173f7112a8854533c0cd846a459
gnuh96 <ntuanhung.96@gmail.com>
2022-03-08 14:37:49 +0100

    Create index.php v1

---
+++ Pagination/Version1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pagination avec PHP</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php 
+        // Traitement des requêtes de base de données et algorithme de pagination 
+        ?>
+        <div>
+            <?php 
+            // Afficher la liste de donnees
+            ?>
+        </div>
+        <div class="pagination">
+           <?php 
+            // Pagination
+           ?>
+        </div>
+    </body>
+</html>
